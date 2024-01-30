c29e6cb6f727e5d24abc086fac142332459bee09
lcolladotor <lcolladotor@gmail.com>
2024-01-30 10:53:26 -0500

    Initial commit

---
+++ R/01-notas.R
@@ -0,0 +1,7 @@
+## Pasos para crear este RStudio proyect
+## con este archivo de notas
+usethis::create_project("~/rnaseq_2024_notas")
+usethis::use_r("01-notas.R")
+
+## Ejercicio 2
+usethis::use_r("02-visualizar-mtcars.R")

dd40f6f2bc2671e6f940ba73281f7a7758ff2037
lcolladotor <lcolladotor@gmail.com>
2024-01-31 14:05:59 -0500

    Ejemplo usando iSEE(initial)

--- R/04_SummarizedExperiment.R
+++ R/04_SummarizedExperiment.R
@@ -113,3 +113,134 @@ rse2 <- rse[, c(1, 4, 6)]
 ls()
 rm(rse2)
 ?gc
+
+
+## Explora el objeto rse de forma interactiva
+library("iSEE")
+iSEE::iSEE(rse)
+
+
+
+
+
+
+initial <- list()
+
+################################################################################
+# Settings for Row data table 1
+################################################################################
+
+initial[["RowDataTable1"]] <- new("RowDataTable", Selected = "gene_101", Search = "", SearchColumns = "",
+    HiddenColumns = character(0), VersionInfo = list(iSEE = structure(list(
+        c(2L, 14L, 0L)), class = c("package_version", "numeric_version"
+    ))), PanelId = c(RowDataTable = 1L), PanelHeight = 500L,
+    PanelWidth = 4L, SelectionBoxOpen = FALSE, RowSelectionSource = "---",
+    ColumnSelectionSource = "---", DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE,
+    ColumnSelectionDynamicSource = FALSE, RowSelectionRestrict = FALSE,
+    ColumnSelectionRestrict = FALSE, SelectionHistory = list())
+
+################################################################################
+# Settings for Feature assay plot 1
+################################################################################
+
+initial[["FeatureAssayPlot1"]] <- new("FeatureAssayPlot", Assay = "logcounts", XAxis = "Column data",
+    XAxisColumnData = "Treatment", XAxisFeatureName = "gene_1",
+    XAxisFeatureSource = "---", XAxisFeatureDynamicSource = FALSE,
+    YAxisFeatureName = "gene_101", YAxisFeatureSource = "RowDataTable1",
+    YAxisFeatureDynamicSource = FALSE, FacetRowByColData = "Treatment",
+    FacetColumnByColData = "Treatment", ColorByColumnData = "Treatment",
+    ColorByFeatureNameAssay = "logcounts", ColorBySampleNameColor = "#FF0000",
+    ShapeByColumnData = "Treatment", SizeByColumnData = NA_character_,
+    TooltipColumnData = character(0), FacetRowBy = "None", FacetColumnBy = "None",
+    ColorBy = "Column data", ColorByDefaultColor = "#000000",
+    ColorByFeatureName = "gene_1", ColorByFeatureSource = "---",
+    ColorByFeatureDynamicSource = FALSE, ColorBySampleName = "A",
+    ColorBySampleSource = "---", ColorBySampleDynamicSource = FALSE,
+    ShapeBy = "None", SizeBy = "None", SelectionAlpha = 0.1,
+    ZoomData = numeric(0), BrushData = list(), VisualBoxOpen = FALSE,
+    VisualChoices = "Color", ContourAdd = FALSE, ContourColor = "#0000FF",
+    PointSize = 1, PointAlpha = 1, Downsample = FALSE, DownsampleResolution = 200,
+    CustomLabels = FALSE, CustomLabelsText = "A", FontSize = 1,
+    LegendPointSize = 1, LegendPosition = "Bottom", HoverInfo = TRUE,
+    LabelCenters = FALSE, LabelCentersBy = "Treatment", LabelCentersColor = "#000000",
+    VersionInfo = list(iSEE = structure(list(c(2L, 14L, 0L)), class = c("package_version",
+    "numeric_version"))), PanelId = c(FeatureAssayPlot = 1L),
+    PanelHeight = 500L, PanelWidth = 8L, SelectionBoxOpen = FALSE,
+    RowSelectionSource = "---", ColumnSelectionSource = "---",
+    DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE, ColumnSelectionDynamicSource = FALSE,
+    RowSelectionRestrict = FALSE, ColumnSelectionRestrict = FALSE,
+    SelectionHistory = list())
+
+################################################################################
+# Settings for Sample assay plot 1
+################################################################################
+
+initial[["SampleAssayPlot1"]] <- new("SampleAssayPlot", Assay = "logcounts", XAxis = "None", XAxisRowData = "feature_id",
+    XAxisSampleName = "A", XAxisSampleSource = "---", XAxisSampleDynamicSource = FALSE,
+    YAxisSampleName = "C", YAxisSampleSource = "ColumnDataTable1",
+    YAxisSampleDynamicSource = FALSE, FacetRowByRowData = NA_character_,
+    FacetColumnByRowData = NA_character_, ColorByRowData = "feature_id",
+    ColorBySampleNameAssay = "logcounts", ColorByFeatureNameColor = "#FF0000",
+    ShapeByRowData = NA_character_, SizeByRowData = NA_character_,
+    TooltipRowData = character(0), FacetRowBy = "None", FacetColumnBy = "None",
+    ColorBy = "None", ColorByDefaultColor = "#000000", ColorByFeatureName = "gene_1",
+    ColorByFeatureSource = "---", ColorByFeatureDynamicSource = FALSE,
+    ColorBySampleName = "A", ColorBySampleSource = "---", ColorBySampleDynamicSource = FALSE,
+    ShapeBy = "None", SizeBy = "None", SelectionAlpha = 0.1,
+    ZoomData = numeric(0), BrushData = list(), VisualBoxOpen = FALSE,
+    VisualChoices = "Color", ContourAdd = FALSE, ContourColor = "#0000FF",
+    PointSize = 1, PointAlpha = 1, Downsample = FALSE, DownsampleResolution = 200,
+    CustomLabels = FALSE, CustomLabelsText = "gene_1", FontSize = 1,
+    LegendPointSize = 1, LegendPosition = "Bottom", HoverInfo = TRUE,
+    LabelCenters = FALSE, LabelCentersBy = NA_character_, LabelCentersColor = "black",
+    VersionInfo = list(iSEE = structure(list(c(2L, 14L, 0L)), class = c("package_version",
+    "numeric_version"))), PanelId = c(SampleAssayPlot = 1L),
+    PanelHeight = 500L, PanelWidth = 4L, SelectionBoxOpen = FALSE,
+    RowSelectionSource = "---", ColumnSelectionSource = "---",
+    DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE, ColumnSelectionDynamicSource = FALSE,
+    RowSelectionRestrict = FALSE, ColumnSelectionRestrict = FALSE,
+    SelectionHistory = list())
+
+################################################################################
+# Settings for Column data table 1
+################################################################################
+
+initial[["ColumnDataTable1"]] <- new("ColumnDataTable", Selected = "C", Search = "", SearchColumns = "",
+    HiddenColumns = character(0), VersionInfo = list(iSEE = structure(list(
+        c(2L, 14L, 0L)), class = c("package_version", "numeric_version"
+    ))), PanelId = c(ColumnDataTable = 1L), PanelHeight = 500L,
+    PanelWidth = 4L, SelectionBoxOpen = FALSE, RowSelectionSource = "---",
+    ColumnSelectionSource = "---", DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE,
+    ColumnSelectionDynamicSource = FALSE, RowSelectionRestrict = FALSE,
+    ColumnSelectionRestrict = FALSE, SelectionHistory = list())
+
+################################################################################
+# Settings for Complex heatmap 1
+################################################################################
+
+initial[["ComplexHeatmapPlot1"]] <- new("ComplexHeatmapPlot", Assay = "logcounts", CustomRows = TRUE,
+    CustomRowsText = "gene_1\ngene_101\n# gene_1000\ngene_150\ngene_200",
+    ClusterRows = TRUE, ClusterRowsDistance = "spearman", ClusterRowsMethod = "ward.D2",
+    DataBoxOpen = FALSE, VisualChoices = "Annotations", ColumnData = "Treatment",
+    RowData = character(0), CustomBounds = FALSE, LowerBound = NA_real_,
+    UpperBound = NA_real_, AssayCenterRows = TRUE, AssayScaleRows = TRUE,
+    DivergentColormap = "purple < black < yellow", ShowDimNames = "Rows",
+    LegendPosition = "Bottom", LegendDirection = "Horizontal",
+    VisualBoxOpen = FALSE, NamesRowFontSize = 10, NamesColumnFontSize = 10,
+    ShowColumnSelection = TRUE, OrderColumnSelection = TRUE,
+    VersionInfo = list(iSEE = structure(list(c(2L, 14L, 0L)), class = c("package_version",
+    "numeric_version"))), PanelId = c(ComplexHeatmapPlot = 1L),
+    PanelHeight = 500L, PanelWidth = 4L, SelectionBoxOpen = FALSE,
+    RowSelectionSource = "---", ColumnSelectionSource = "---",
+    RowSelectionDynamicSource = FALSE, ColumnSelectionDynamicSource = FALSE,
+    RowSelectionRestrict = FALSE, ColumnSelectionRestrict = FALSE,
+    SelectionHistory = list())
+
+iSEE::iSEE(rse, initial = initial, appTitle = "iSEE pre configurada")
+
+
+## Descarguemos unos datos de spatialLIBD
+sce_layer <- spatialLIBD::fetch_data("sce_layer")
+## Revisemos el tamaño de este objeto
+lobstr::obj_size(sce_layer)
+iSEE::iSEE(sce_layer)

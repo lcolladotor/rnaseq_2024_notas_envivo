2c31d1de56b6737487e45b959f9b4ccbacfffa15
lcolladotor <lcolladotor@gmail.com>
2024-01-31 10:54:08 -0500

    Notas en vivo hasta la parte del primer ejercicio

--- R/04_SummarizedExperiment.R
+++ R/04_SummarizedExperiment.R
@@ -1,3 +1,5 @@
+## Código de https://lcolladotor.github.io/rnaseq_LCG-UNAM_2024/objetos-de-bioconductor-para-datos-de-expresi%C3%B3n.html
+
 ## Lets build our first SummarizedExperiment object
 library("SummarizedExperiment")
 ## ?SummarizedExperiment
@@ -12,10 +14,14 @@ ncols <- 6
 set.seed(20210223)
 counts <- matrix(runif(nrows * ncols, 1, 1e4), nrows)
 ## Información de nuestros genes
-rowRanges <- GRanges(
+rowRanges <- GenomicRanges::GRanges(
     rep(c("chr1", "chr2"), c(50, 150)),
-    IRanges(floor(runif(200, 1e5, 1e6)), width = 100),
-    strand = sample(c("+", "-"), 200, TRUE),
+    IRanges::IRanges(floor(runif(200, 1e5, 1e6)), width = 100),
+    strand = sample(
+        x = c("+", "-"),
+        size = 200,
+        replace = TRUE
+    ),
     feature_id = sprintf("ID%03d", 1:200)
 )
 names(rowRanges) <- paste0("gene_", seq_len(length(rowRanges)))
@@ -33,3 +39,22 @@ rse <- SummarizedExperiment(
 
 ## Exploremos el objeto resultante
 rse
+
+## Algunos ejemplos de comandos para explorar nuestro
+## objeto de RangedSummarizedExperiment (RSE)
+dim(rse)
+assayNames(rse)
+head(assay(rse))
+rowRanges(rse)
+mcols(rowRanges(rse))
+rowData(rse)
+rowData(rse)$feature_id[3]
+colData(rse)
+colData(rse)$Treatment[4]
+rse$Treatment[4]
+
+
+## Comando 1
+rse[1:2, ]
+## Comando 2
+rse[, c("A", "D", "F")]

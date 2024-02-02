d4a1a5fcd7b8c192dade647a1b3bdfb1616223b8
lcolladotor <lcolladotor@gmail.com>
2024-02-02 15:08:31 -0500

    Notas con los datos de ejemplo de SPEAQeasy

---
+++ R/08_notas_repaso.R
@@ -0,0 +1,6 @@
+speaqeasy_data <- file.path(tempdir(), "rse_speaqeasy.RData")
+download.file("https://github.com/LieberInstitute/SPEAQeasy-example/blob/master/rse_speaqeasy.RData?raw=true", speaqeasy_data, mode = "wb")
+library("SummarizedExperiment")
+load(speaqeasy_data, verbose = TRUE)
+
+rse_gene
